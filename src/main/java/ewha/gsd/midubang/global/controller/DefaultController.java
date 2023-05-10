292f75b335eadc24f8e93c940430cdebd9867c91
Sooah Han <s2sooas2@ewhain.net>
2023-05-10 22:29:05 +0900

    feat: api for health check

---
+++ src/main/java/ewha/gsd/midubang/global/controller/DefaultController.java
@@ -0,0 +1,17 @@
+package ewha.gsd.midubang.global.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DefaultController {
+
+    @GetMapping("/health")
+    public ResponseEntity healthCheck() {
+        return ResponseEntity.ok(null);
+    }
+    
+}
