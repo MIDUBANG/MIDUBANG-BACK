c5d7026b6f995ae22977f89150391d0e81afb41b
Sooah Han <s2sooas2@ewhain.net>
2023-05-24 17:16:09 +0900

    chore: edit CORS config

--- src/main/java/ewha/gsd/midubang/global/config/WebConfig.java
+++ src/main/java/ewha/gsd/midubang/global/config/WebConfig.java
@@ -6,13 +6,23 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     /* [DB] password 암호화 */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // CORS를 적용할 URL 패턴 정의
+                .allowedOrigins("https://www.midubang.com") // 자원 공유를 허락할 Origin 지정
+                .allowedMethods("GET", "POST", "DELETE", "PUT") // 허용할 HTTP method 지정
+                .maxAge(3000); // 설정 시간만큼 pre-flight 리퀘스트 캐싱
     }
 }
